2564e50239c0f8aa7a5d834b6ca6f59d3b0ccd46
dkmqflx <dkmqflx@gmail.com>
2025-03-30 13:34:12 +0900

    7 - Layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,38 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>This is a header</Text>
+      </View>
+
+      <Slot />
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>This is a footer</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  headerText: {
+    color: "white",
+  },
+  footer: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  footerText: {
+    color: "white",
+  },
+});
---
+++ app/products/_layout.tsx
@@ -0,0 +1,27 @@
+import { Slot } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function ProductsLayout() {
+  return (
+    <View style={styles.container}>
+      <Slot />
+
+      <View style={styles.discountedProducts}>
+        <Text>Products Layout</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  discountedProducts: {
+    backgroundColor: "orange",
+    padding: 20,
+    width: "100%",
+  },
+});
